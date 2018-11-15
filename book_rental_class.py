a9c8593c00e5c26c01ceaee182c41bbc1271b170
icciaa <31885918+icciaa@users.noreply.github.com>
2018-11-15 17:22:03 -0500

    Add files via upload

--- book_rental_class.py
+++ book_rental_class.py
@@ -1,7 +1,7 @@
 class BookRental:
     # Creating Book Rental class
 
-    def __init__(self, isbn, book_title, author, rental_id, owner_id, rental_value, start_date, end_date):
+    def __init__(self, isbn, book_title, author, rental_id, owner_id, rental_value, start_date, end_date,):
         self.isbn = isbn
         self.book_title = book_title
         self.author = author
@@ -14,13 +14,13 @@ class BookRental:
     # Function to pass in values from the ISBN search and set variables needed for posting book rental
 
     def get_rental_info(self):
-        self.isbn = input("ISBN ")  # pass ISBN from search and set in this variable
-        self.author = input("author")  # pass Author from search and set in this variable
-        self.book_title = input("Title")  # pass title from search and set in this variable
-        self.owner_id = "owner id"  # no variable to be passed yet. This would come from login group
-        self.rental_id = input("Enter rental ID")  # Owner assigns rental value to book
-        self.rental_value = input("Enter a rental value")  # Owner sets the start and end period for book rental
-        self.start_date = input(" Enter your rental period start date: (format mm/dd/yyyy) ")
+        self.isbn = input("ISBN: ")  # pass ISBN from search and set in this variable
+        self.author = input("author: ")  # pass Author from search and set in this variable
+        self.book_title = input("Title: ")  # pass title from search and set in this variable
+        self.owner_id = "owner id: "  # no variable to be passed yet. This would come from login group
+        self.rental_id = input("Enter rental ID: ")  # Owner assigns rental value to book
+        self.rental_value = input("Enter a rental value: ")  # Owner sets the start and end period for book rental
+        self.start_date = input(" Enter your rental period start date: ")
         self.end_date = input(" Enter your rental period end date: ")
 
     # Creating an instance of the class 'BookRental' and setting the variables. This will essentially create a
--- func_post_book.py
+++ func_post_book.py
@@ -1,8 +1,14 @@
 import json
 # creating the json object that will serve the information to the webpage
 
+# function to post book
+
 
 def post_book(book_rental_info):
+    # By default owner status and rental status are set at time of book posting
+    owner_status = "active"
+    rental_status = "checked in"
+    # Gets user input for book posting and converts to json string
     x = {
         "isbn": str(book_rental_info.isbn),
         "book author": str(book_rental_info.author),
@@ -11,11 +17,47 @@ def post_book(book_rental_info):
         "owner id": str(book_rental_info.owner_id),
         "rental value": str(book_rental_info.rental_value),
         "start date": str(book_rental_info.start_date),
-        "end date": str(book_rental_info.end_date)
+        "end date": str(book_rental_info.end_date),
+        "owner status": str(owner_status),
+        "rental status": str(rental_status)
     }
     # convert into JSON:
 
     book_post = json.dumps(x)
     # result is a JSON string
+    # creates book file or appends book file if it exists with the posting information
 
-    print(book_post)
+    try:
+        file = open('books.txt', 'r')
+        file_exists = str('yes')
+        file.close()
+    except FileNotFoundError:
+        file_exists = str('no')
+
+    if file_exists == 'no':
+        file = open("books.txt", "w+")
+        file.close()
+
+    file = open("books.txt", "a")
+    file.write("\n" + book_post)
+    file.close()
+    file = open("books.txt", "r")
+    book_contents = file.read()
+    # print(book_contents)
+    # print(book_post)
+    return book_contents
+
+# put book file into list format for searching and to updated with rented book
+
+# function for getting the rental ID. This will be replaced with an API call
+# to the student renters group
+
+
+def input_rental_id():
+    rental_id = input("Enter rental ID: ")
+    return rental_id
+
+
+
+
+
